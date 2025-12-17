84a389ed99e7f6f3e38b7b740bd1e31ed353a1f4
Paul-Elliot <peada@free.fr>
2025-12-16 23:47:18 -0500

    Wordhack progress

--- wordhack/engine/siteswap.ml
+++ wordhack/engine/siteswap.ml
@@ -6,8 +6,21 @@ let other_hand = function Left -> Right | Right -> Left
 (* type landing = { hand : hand; time : float } *)
 type ball_state = {
   ball : ball;
-  position : float * float (* ; landing : landing *);
+  mutable position : float * float (* ; landing : landing *);
+  offset : float * float;
 }
+
+let set_position (ball_state : ball_state) position =
+  let { ball; position = _; offset } = ball_state in
+  let x = fst position -. fst offset in
+  let y = snd position -. snd offset in
+  let style =
+    let open Jstr in
+    v "translate(" + Jstr.of_float x + v "px," + Jstr.of_float y + v "px)"
+  in
+  Brr.Console.(log [ style ]);
+  Brr.El.set_inline_style (Jstr.v "transform") style ball;
+  ball_state.position <- (x, y)
 
 type timing = Empty | Ball of ball_state
 
@@ -18,6 +31,8 @@ module Ring = struct
     let index = 0 and length = Array.length array in
     { array; index; length }
 
+  let get x n = x.array.((x.index + n) mod x.length)
+
   let pop x =
     let index = x.index in
     let head = x.array.(index) in
@@ -25,6 +40,13 @@ module Ring = struct
     head
 
   let set x n v = x.array.((x.index + n) mod x.length) <- v
+
+  let foldi f acc x =
+    let acc = ref acc in
+    for i = 0 to x.length - 1 do
+      acc := f i !acc (get x i)
+    done;
+    !acc
 end
 
 type state = {
@@ -57,7 +79,9 @@ let square_interpolate current_y target_y t delta_t =
   +. ((target_y -. current_y +. (g *. t *. t)) /. t *. delta_t)
   +. current_y
 
-let hand_position = function Left -> (_ : float * float) | Right -> _
+let hand_position = function
+  | Left -> ((500., 500.) : float * float)
+  | Right -> ((500., 500.) : float * float)
 
 let place hand n time delta_time (current_x, current_y) =
   let hand = if n mod 2 = 0 then hand else other_hand hand in
@@ -66,34 +90,76 @@ let place hand n time delta_time (current_x, current_y) =
   let y = square_interpolate current_y target_y time delta_time in
   (x, y)
 
+let step_time = 1000.
+
+let update_view state delta_time =
+  let () =
+    Ring.foldi
+      (fun i () ball ->
+        match ball with
+        | Empty -> ()
+        | Ball ball_state ->
+            let new_position =
+              let ( !! ) = float_of_int in
+              place Left 0
+                ((!!i *. step_time) +. state.timer)
+                delta_time ball_state.position
+            in
+            set_position ball_state new_position)
+      () state.state
+  in
+  ()
+
 let next state time_spent =
   state.timer <- state.timer -. time_spent;
-  if state.timer < 0. then
+  if state.timer < 0. then (
+    Brr.Console.(log [ "Activated step" ]);
+    state.timer <- state.timer +. step_time;
     let ball = Ring.pop state.state in
     let number = Ring.pop state.siteswap in
     match (ball, number) with
     | Empty, 0 -> ()
     | (Ball _ as b), n when n > 0 -> Ring.set state.state n b
-    | _ -> assert false
-  else ()
+    | _ -> assert false)
+  else ();
+  update_view state time_spent
 
 let siteswap = Ring.create [| 4; 4; 1 |]
 
 let timing =
-  let position = assert false and ball = assert false in
   Ring.create
     [|
-      Ball { position; ball }; Ball { position; ball }; Ball { position; ball };
+      Ball
+        {
+          position = (10., 10.);
+          ball = Brr.El.find_first_by_selector (Jstr.v "#id1") |> Option.get;
+          offset = (100., 100.);
+        };
+      Ball
+        {
+          position = (100., 100.);
+          ball = Brr.El.find_first_by_selector (Jstr.v "#id2") |> Option.get;
+          offset = (100., 100.);
+        };
+      Ball
+        {
+          position = (100., 100.);
+          ball = Brr.El.find_first_by_selector (Jstr.v "#id3") |> Option.get;
+          offset = (100., 100.);
+        };
     |]
 
-let now () = assert false
-let nextFrame _ = assert false
+let now () = Brr.Performance.now_ms Brr.G.performance
 
 let loop () =
   let state = { timer = 0.; state = timing; siteswap } in
 
   let rec update old_now now =
     next state (now -. old_now);
-    nextFrame (update now)
+    let _ = Brr.G.request_animation_frame (update now) in
+    ()
   in
-  nextFrame (update (now ()))
+  let _ = Brr.G.request_animation_frame (update (now ())) in
+  ()
+
+let () = loop ()
